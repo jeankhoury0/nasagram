d787d4d8882c8bd7461a037dd0de4ccae60622ce
jeankhoury0 <jeankhoury0@gmail.com>
2022-01-17 16:27:21 -0500

    filter

--- src/components/imagePost/imagePost.js
+++ src/components/imagePost/imagePost.js
@@ -8,26 +8,16 @@ export default function ImagePost(props) {
     function toogleLike(){
         setLike(prevCount => {
             if (prevCount.fa == "far"){
-                return {fa: "fas"}
+                return {fa: "fas bg-pink-700 text-white"}
             }
             return {fa: "far"}
         })   
         
            
     }
-    const data = props.data || (
-         {
-             "copyright": "Helmut Eder",
-             "date": "2021-06-09",
-             "explanation": "This moon appears multiply strange.  This moon was a full moon, specifically called a Flower Moon at this time of the year.  But that didn't make it strange -- full moons occur once a month (moon-th).  This moon was a supermoon, meaning that it reached its full phase near its closest approach to the Earth in its slightly elliptical orbit.  Somewhat strange, a supermoon appears a bit larger and brighter than the average full moon -- and enables it to be called a Super Flower Moon.  This moon was undergoing a total lunar eclipse. An eclipsed moon can look quite strange, being dark, unevenly lit, and, frequently, red -- sometimes called blood red. Therefore, this moon could be called a Super Flower Blood Moon. This moon was seen through thin clouds. These clouds created a faint corona around the moon, making it look not only strange, but colorful.  This moon was imaged so deeply that the heart of the Milky Way galaxy, far in the background, was visible to its lower right.  This moon, this shadow, this galaxy and these colors were all captured last month near Cassilis, NSW, Australia -- with a single shot. (Merged later with two lower shots that better capture the Milky Way.)   Details: Annular Solar Eclipse Tomorrow  Gallery: Total Eclipse of the Super Flower Blood Moon",
-             "hdurl": "https://apod.nasa.gov/apod/image/2106/MultiEclipse_Eder_1080.jpg",
-             "media_type": "image",
-             "service_version": "v1",
-             "title": "A Total Lunar Eclipse Corona",
-             "url": "https://apod.nasa.gov/apod/image/2106/MultiEclipse_Eder_960.jpg"
-         }
+    const data = props.data 
   
-     )
+     
      console.log(data)
     return(
         <div className="shadow-md bg-white rounded mx-2 break-inside-avoid">
@@ -37,7 +27,7 @@ export default function ImagePost(props) {
                     <img src={data.url} className="object-cover aspect-square w-[100%] h-100"></img>
                 }
                 {data.media_type == "video" && 
-                    <iframe src={`${data.url}?autoplay=1&mute=1`} className="object-cover aspect-square w-[100%] h-100" controls autoplay>
+                    <iframe src={`${data.url}?autoplay=1&mute=1`} className="object-cover aspect-square w-[100%] h-100" controls autoPlay>
                   </iframe>
                 }                
             </div>
@@ -48,9 +38,9 @@ export default function ImagePost(props) {
                     <p> {data.date} </p>
                 </div>
                 <div className="flex justify-between grow items-stretch">
-                    <button className={`${like.fa} fa-heart text-2xl bg-pink-700 text-white grow`} alt="Like the picture" onClick={toogleLike}>
+                    <button className={`${like.fa} fa-heart text-2xl   grow transition ease-in-out delay-150 hover:bg-pink-700 focus:bg-pink-700 hover:text-white focus:text-white`} alt="Like the picture" onClick={toogleLike}>
                     </button>
-                    <button className="fas fa-link text-2xl bg-blue-700 text-white grow" alt="Get link to share"></button>
+                    <button className="fas fa-link text-2xl hover:bg-blue-700 focus:bg-blue-700 hover:text-white focus:text-white grow" alt="Get link to share"></button>
 
                 </div>
                 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,38 @@ import ImagePost from "../components/imagePost/imagePost"
 import Loading from "../components/loading"
 // markup
 class IndexPage extends React.Component {
+  constructor(){
+    super()
+    this.state= {
+      loading: true,
+      error: false,
+      filter: 
+      {
+      startDate: "",
+      endDate: "",
+      count: "10"
+    },
+      data: []
+    }
 
-  state= {
-    loading: true,
-    error: false,
-    data: []
+    this.setFilters = this.setFilters.bind(this)
+    this.setState = this.setState.bind(this)
+
   }
 
-  componentDidMount(){
-    var url = new URL("https://api.nasa.gov/planetary/apod")
-    const API_KEY = "mPPJDe7A3eOPnUtTr8MWWfrWvLdJg8HuhEgVmiAW"
-    var params= {
-      "api_key": API_KEY,
-      "count": 20
-    }
-    url.search = new URLSearchParams(params).toString()
-    fetch(url)
+  URL = new URL("https://api.nasa.gov/planetary/apod")
+  API_KEY = "mPPJDe7A3eOPnUtTr8MWWfrWvLdJg8HuhEgVmiAW"
+  params= {
+    "api_key": this.API_KEY,
+    "count": "10"
+  } 
+
+
+  async fetchData(){
+
+    this.URL.search = new URLSearchParams(this.params).toString()
+
+    fetch(this.URL)
       .then(res =>{
         return res.json()})
       .then(json =>{
@@ -30,19 +46,74 @@ class IndexPage extends React.Component {
           loading: false
         })
       })
-
   }
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+     if (prevState.filter !== this.state.filter){
+       console.log("ENTERED")
+       this.params= {
+        "api_key": this.API_KEY,
+        "start_date": this.state.filter.startDate,
+        "end_date": this.state.filter.endDate,
+        "count" : this.state.filter.count
+      } 
+      this.fetchData()
+     }
+   }
+
+  
+
+  
+  setFilters(){
+    var startDate = document.getElementById("inputStartDate").value
+    var endDate = document.getElementById("inputEndDate").value
+    var inputCount = document.getElementById("inputCount").value
+    console.log(startDate, endDate, document.getElementById("inputStartDate").value)
+    this.setState({
+      loading: true,
+      filter : {
+        startDate: startDate || "",
+        endDate: endDate || "",
+        count: inputCount || "23"
+      }
+    })
+  }
+  
 
   render(){
     const {loading, data} = this.state
     return(
       <><Helmet>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Nasagram</title>
       </Helmet>
+
       <div className="text-center bg-opacity-90 py-4 bg-white sticky top-0 left-0 right-0 text-2xl">
-        <h1 className="">Nasa Gram</h1>
+        <h1 className="">Nasagram</h1>
         <a href="https://github.com/jeankhoury0/nasagram" target="_blank" ref="nofollow"> <li className="fab fa-github px-2"></li>Check the code</a>
       </div>
+      {/* Filter */}
+      <div className="container grid gap-3 bg-slate-300 ">
+        <h2>Filter</h2>
+        <div>
+          <label htmlFor="inputStartDate" className="p-3">Start Date</label>
+          <input type="date" id="inputStartDate" min="07-01-1995" max={new Date().toISOString().split("T")[0]}></input>
+        </div>
+        <div>
+          <label htmlFor="inputEndDate" className="p-3">End Date</label>
+          <input type="date" id="inputEndDate"  min="07-01-1995" max={new Date().toISOString().split("T")[0]}></input>
+        </div>
+        <div>
+          <label htmlFor="inputCount" className="p-3">Count</label>
+          <input type="number" id="inputCount" max="23" ></input>
+        </div>
+        <a onClick={this.setFilters}>refresh</a>
+      </div>
+      {/* End filter */}
       <main className="flex self-center items-center" id="root">
           <div className="columns-xs gap-8 space-y-8 py-2">
             {loading ? <Loading></Loading> : (
